483588b4ffca2b4515e7b248b4f80e32d21c7b6f
Jordan GRILLY <jordangrilly@gmail.com>
2026-09-12 18:54:10 +0200

    Tests: validate migrated gameplay wiring

--- Assets/Tests/PlayMode/PoolTableScenePlayModeSmokeTests.cs
+++ Assets/Tests/PlayMode/PoolTableScenePlayModeSmokeTests.cs
@@ -1,4 +1,7 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,9 +12,76 @@ namespace PoolTable.Tests.PlayMode
     public sealed class PoolTableScenePlayModeSmokeTests
     {
         private const string PoolTableScenePath = "Assets/Scenes/PoolTable.unity";
+        private static readonly HashSet<string> BilliardBallTags = new HashSet<string>
+        {
+            "white",
+            "black",
+            "filled",
+            "striped",
+        };
 
         [UnityTest]
         public IEnumerator PoolTableScene_StartsAndKeepsEssentialSceneObjectsAvailable()
+        {
+            yield return LoadPoolTableScene();
+
+            var activeScene = SceneManager.GetActiveScene();
+            Assert.That(activeScene.path, Is.EqualTo(PoolTableScenePath));
+            Assert.That(activeScene.isLoaded, Is.True);
+
+            Assert.That(GameObject.Find("Main Camera"), Is.Not.Null);
+            Assert.That(SceneContainsObject(activeScene, "POOL TABLE"), Is.True);
+            Assert.That(SceneContainsObject(activeScene, "Balls"), Is.True);
+            Assert.That(SceneContainsObject(activeScene, "PoolCue"), Is.True);
+            Assert.That(SceneContainsObject(activeScene, "Player"), Is.True);
+        }
+
+        [UnityTest]
+        public IEnumerator PoolTableScene_InitializesLegacyGameplayWiring()
+        {
+            yield return LoadPoolTableScene();
+
+            var activeScene = SceneManager.GetActiveScene();
+            var gameManager = FindMonoBehaviourByTypeName(activeScene, "GameManager");
+            var playerController = FindMonoBehaviourByTypeName(activeScene, "PlayersStateManagement");
+
+            Assert.That(gameManager, Is.Not.Null, "GameManager must be initialized in the gameplay scene.");
+            Assert.That(playerController, Is.Not.Null, "PlayersStateManagement must be initialized in the gameplay scene.");
+
+            var cueBall = GetPublicGameObjectField(playerController, "WhiteBall");
+            var cueCamera = GetPublicGameObjectField(playerController, "Cue_Camera");
+
+            Assert.That(cueBall, Is.Not.Null);
+            Assert.That(cueBall.CompareTag("white"), Is.True);
+            Assert.That(cueBall.GetComponent<Rigidbody>(), Is.Not.Null);
+            Assert.That(cueBall.GetComponent<SphereCollider>(), Is.Not.Null);
+            Assert.That(HasMonoBehaviourType(cueBall, "BallStateManager"), Is.True);
+
+            Assert.That(cueCamera, Is.Not.Null);
+            Assert.That(cueCamera.name, Is.EqualTo("Camera_Cue"));
+
+            var billiardBalls = EnumerateSceneObjects(activeScene)
+                .Where(gameObject => BilliardBallTags.Contains(gameObject.tag))
+                .ToArray();
+
+            Assert.That(billiardBalls, Has.Length.EqualTo(16));
+
+            foreach (var ball in billiardBalls)
+            {
+                Assert.That(ball.GetComponent<Rigidbody>(), Is.Not.Null, $"{ball.name} must keep its Rigidbody.");
+                Assert.That(ball.GetComponent<SphereCollider>(), Is.Not.Null, $"{ball.name} must keep its SphereCollider.");
+                Assert.That(HasMonoBehaviourType(ball, "BallStateManager"), Is.True, $"{ball.name} must keep BallStateManager.");
+            }
+
+            var playerOneTurn = GetPublicGameObjectField(gameManager, "UI_Player1Turn");
+            var playerTwoTurn = GetPublicGameObjectField(gameManager, "UI_Player2Turn");
+
+            Assert.That(playerOneTurn, Is.Not.Null);
+            Assert.That(playerTwoTurn, Is.Not.Null);
+            Assert.That(playerOneTurn.activeSelf, Is.Not.EqualTo(playerTwoTurn.activeSelf), "Exactly one turn indicator must be active after startup.");
+        }
+
+        private static IEnumerator LoadPoolTableScene()
         {
             Assert.That(SceneManager.sceneCountInBuildSettings, Is.GreaterThan(0));
 
@@ -26,32 +96,44 @@ namespace PoolTable.Tests.PlayMode
             yield return null;
             yield return null;
             yield return null;
-
-            var activeScene = SceneManager.GetActiveScene();
-            Assert.That(activeScene.path, Is.EqualTo(PoolTableScenePath));
-            Assert.That(activeScene.isLoaded, Is.True);
-
-            Assert.That(GameObject.Find("Main Camera"), Is.Not.Null);
-            Assert.That(SceneContainsObject(activeScene, "POOL TABLE"), Is.True);
-            Assert.That(SceneContainsObject(activeScene, "Balls"), Is.True);
-            Assert.That(SceneContainsObject(activeScene, "PoolCue"), Is.True);
-            Assert.That(SceneContainsObject(activeScene, "Player"), Is.True);
         }
 
         private static bool SceneContainsObject(Scene scene, string objectName)
+        {
+            return EnumerateSceneObjects(scene).Any(gameObject => gameObject.name == objectName);
+        }
+
+        private static IEnumerable<GameObject> EnumerateSceneObjects(Scene scene)
         {
             foreach (var rootObject in scene.GetRootGameObjects())
             {
                 foreach (var transform in rootObject.GetComponentsInChildren<Transform>(true))
                 {
-                    if (transform.name == objectName)
-                    {
-                        return true;
-                    }
+                    yield return transform.gameObject;
                 }
             }
+        }
 
-            return false;
+        private static MonoBehaviour FindMonoBehaviourByTypeName(Scene scene, string typeName)
+        {
+            return EnumerateSceneObjects(scene)
+                .SelectMany(gameObject => gameObject.GetComponents<MonoBehaviour>())
+                .FirstOrDefault(component => component != null && component.GetType().Name == typeName);
+        }
+
+        private static bool HasMonoBehaviourType(GameObject gameObject, string typeName)
+        {
+            return gameObject.GetComponents<MonoBehaviour>()
+                .Any(component => component != null && component.GetType().Name == typeName);
+        }
+
+        private static GameObject GetPublicGameObjectField(MonoBehaviour component, string fieldName)
+        {
+            var field = component.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.Public);
+            Assert.That(field, Is.Not.Null, $"{component.GetType().Name}.{fieldName} must remain a public serialized field during migration.");
+            Assert.That(field.FieldType, Is.EqualTo(typeof(GameObject)));
+
+            return field.GetValue(component) as GameObject;
         }
     }
 }
